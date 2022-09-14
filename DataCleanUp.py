b038d059780b781e76401b7386767c79a0ba6646
ahmedabdurahim <110900404+ahmedabdurahim@users.noreply.github.com>
2022-09-14 10:27:15 +0300

    Create DataCleanUp.py

---
+++ DataCleanUp.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+class DataCleanUp:
+  df = pd.read_csv("/content/agro_clean_data.csv")
+
+  #Count how much a string in a cell appears in the whole file
+
+  def CountAppearances():
+    counter = 0
+    for i in range(Rows):
+      
+      ######################################################################
+      # Modify these parameters running code or it will generate an error! #
+      ######################################################################
+      
+      if (df.at[i,'ColumnName'] == "string to look for"):
+        counter = counter + 1
+
+    if(counter > 1):
+      print("The string has appeared " + str(counter) + " times in this spreadsheet")
+    elif(counter == 1):
+      print("The string has appeared only once")
+    else:
+      ("Could not find the string")
+
+
+ #Change the character from a cell to a different char
+
+  def FixCells(ColumnName, OldCharacter, NewCharacter):
+    df[ColumnName] = df[ColumnName].str.replace(OldCharacter,NewCharacter)
+
+#Change a cell that contains dual data to a data that comes after a delimeter
+
+  def CleanDualData(ColumnName, Delimiter):
+    df[ColumnName] = df[ColumnName].str.split(Delimiter).str[0]
+
+FixCells('Phone_Number','09', '9')
+FixCells('Phone_Number', '011', '11')
+CleanDualData('Phone_Number', '/')
+
+print(df)
+
+
+
+
+
